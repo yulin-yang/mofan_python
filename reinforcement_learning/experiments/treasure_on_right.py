cf740bb469e5b9a8174d0d4bfca9223c8af4834d
NickTai <1262022832@qq.com>
2023-03-14 23:35:11 +0800

    台式机测试

--- reinforcement_learning/experiments/treasure_on_right.py
+++ reinforcement_learning/experiments/treasure_on_right.py
@@ -1,10 +1,3 @@
-"""
-A simple example for Reinforcement Learning using table lookup Q-learning method.
-An agent "o" is on the left of a 1 dimensional world, the treasure is on the rightmost location.
-Run this program and to see how the agent will improve its strategy of finding the treasure.
-
-View more on my tutorial page: https://morvanzhou.github.io/tutorials/
-"""
 
 import numpy as np
 import pandas as pd
